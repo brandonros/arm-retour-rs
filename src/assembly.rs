d71e9a4bc5278a56675288a2887073b09980823b
Brandon Ros <brandonros1@gmail.com>
2023-08-17 19:37:21 -0400

    checkin

---
+++ src/assembly.rs
@@ -0,0 +1,93 @@
+#![allow(non_snake_case)]
+
+use bitfield::bitfield;
+
+bitfield! {
+    pub struct Movw(u32);
+    impl Debug;
+    u32;
+    pub get_op3, set_op3: 31, 31;
+    pub get_imm3, set_imm3: 30, 28;
+    pub get_Rd, set_Rd: 27, 24;
+    pub get_imm8, set_imm8: 23, 16;
+    pub get_op1, set_op1: 15, 11;
+    pub get_imm1, set_imm1: 10, 10;
+    pub get_op2, set_op2: 9, 4;
+    pub get_imm4, set_imm4: 3, 0;
+}
+
+bitfield! {
+    pub struct Movt(u32);
+    impl Debug;
+    u32;
+    pub get_op3, set_op3: 31, 31;
+    pub get_imm3, set_imm3: 30, 28;
+    pub get_Rd, set_Rd: 27, 24;
+    pub get_imm8, set_imm8: 23, 16;
+    pub get_op1, set_op1: 15, 11;
+    pub get_imm1, set_imm1: 10, 10;
+    pub get_op2, set_op2: 9, 4;
+    pub get_imm4, set_imm4: 3, 0;
+}
+
+bitfield! {
+    pub struct BlxRegister(u16);
+    impl Debug;
+    u16;
+    pub get_opcode1, set_opcode1: 15, 7;
+    pub get_Rm, set_Rm: 6, 3;
+    pub get_opcode2, set_opcode2: 2, 0;
+}
+
+
+fn get_bit_range(input: u16, start: u8, num_bits: u8) -> u16 {
+    let mask = (1u16 << num_bits) - 1;
+    (input >> start) & mask
+}
+
+pub fn encode_movw(immediate: u16, rd: u8) -> u32 {
+    println!("encode_movw immediate = {immediate:x} rd = {rd:x}");
+    let mut movw = Movw(0);
+    movw.set_op1(0b11110);
+    movw.set_imm1(get_bit_range(immediate, 11, 1) as u32); // 11-11
+    movw.set_op2(0b100100);
+    movw.set_imm4(get_bit_range(immediate, 12, 4) as u32); // 12-15
+    movw.set_imm3(get_bit_range(immediate, 8, 3) as u32); // 8-10
+    movw.set_Rd(rd as u32);
+    movw.set_imm8(get_bit_range(immediate, 0, 8) as u32); // 0-7
+    movw.set_op3(0);
+    return u32::from_le_bytes(movw.0.to_be_bytes());
+}
+
+pub fn encode_movt(immediate: u16, rd: u8) -> u32 {
+    println!("encode_movt immediate = {immediate:x} rd = {rd:x}");
+    let mut movt = Movt(0);
+    movt.set_op1(0b11110);
+    movt.set_imm1(get_bit_range(immediate, 11, 1) as u32); // 11-11
+    movt.set_op2(0b101100);
+    movt.set_imm4(get_bit_range(immediate, 12, 4) as u32); // 12-15
+    movt.set_imm3(get_bit_range(immediate, 8, 3) as u32); // 8-10
+    movt.set_Rd(rd as u32);
+    movt.set_imm8(get_bit_range(immediate, 0, 8) as u32); // 0-7
+    movt.set_op3(0);
+    return u32::from_le_bytes(movt.0.to_be_bytes());
+}
+
+pub fn encode_blx_register(rm: u8) -> u16 {
+    println!("encode_blx_register rm = {rm:x}");
+    let mut blx_register = BlxRegister(0);
+    blx_register.set_Rm(rm as u16);
+    blx_register.set_opcode1(0b010001111);
+    blx_register.set_opcode2(0);
+    return u16::from_le_bytes(blx_register.0.to_be_bytes());
+}
+
+pub fn encode_bx() -> u16 {
+    // TODO: bitfield for clarity + support something other than r12
+    return 0x6047; // bx r12
+}
+
+pub fn encode_nop() -> u16 {
+    // TODO: bitfield for clarity?
+    return 0x00bf;
+}
---
+++ src/hooks.rs
@@ -0,0 +1,88 @@
+use std::ffi::*;
+
+use libc::{mprotect, PROT_READ, PROT_WRITE, PROT_EXEC};
+
+use crate::assembly;
+
+pub struct Hook {
+    pub ori_fn_address: usize,
+    pub hook_fn_address: usize,
+    pub ori_fn_bytes: Vec<u8>,
+    pub patch_fn_bytes: Vec<u8>
+}
+
+pub fn enable_hook(ori_fn_address: usize, patch_fn_bytes: &Vec<u8>) {
+    patch_function_memory((ori_fn_address - 1) as *const c_void as *mut u8, &patch_fn_bytes);
+}
+
+pub fn disable_hook(ori_fn_address: usize, ori_fn_bytes: &Vec<u8>) {
+    patch_function_memory((ori_fn_address - 1) as *const c_void as *mut u8, &ori_fn_bytes);
+}
+
+pub fn build_hook(ori_fn_address: *const c_void, hook_fn_address: *const c_void, ori_fn_bytes_length: usize) -> Hook {
+    log::info!("build_hook ori_fn_address = {ori_fn_address:p} hook_fn_address = {hook_fn_address:p}");
+    // backup
+    let ori_fn_bytes = backup_original_bytes(unsafe { ori_fn_address.sub(1) }, ori_fn_bytes_length); // watch out for stupid thumb -1 thing
+    log::info!("build_hook ori_fn_bytes = {:02x?}", ori_fn_bytes);
+    // assemble
+    let register = 12; // r12 / ip
+    let movw = assembly::encode_movw((hook_fn_address as usize) as u16, register);
+    let movt = assembly::encode_movt(((hook_fn_address as usize) >> 16) as u16, register);
+    let bx = assembly::encode_bx();
+    let mut patch_fn_bytes = vec![];
+    patch_fn_bytes.extend_from_slice(&movw.to_be_bytes());
+    patch_fn_bytes.extend_from_slice(&movt.to_be_bytes());
+    patch_fn_bytes.extend_from_slice(&bx.to_be_bytes());
+    if ori_fn_bytes_length == 10 {
+        // no need to pad for nops?
+    } else if ori_fn_bytes_length == 12 {
+        let nop = assembly::encode_nop();
+        patch_fn_bytes.extend_from_slice(&nop.to_be_bytes());
+    } else {
+        panic!("TODO");
+    }
+    log::info!("build_hook patch_fn_bytes = {:02x?}", patch_fn_bytes);
+    // return
+    return Hook {
+        ori_fn_address: ori_fn_address as usize,
+        hook_fn_address: hook_fn_address as usize,
+        ori_fn_bytes: ori_fn_bytes,
+        patch_fn_bytes: patch_fn_bytes
+    };
+}
+
+fn patch_function_memory(target_func: *mut u8, patch_instructions: &[u8]) {
+    // Change memory protection to make it writable
+    let page_size = 4096; // Often 4KB, but you should retrieve this dynamically.
+    let page_aligned_addr = (target_func as usize & !(page_size - 1)) as *mut c_void;
+    unsafe {
+        let ret_val = mprotect(
+            page_aligned_addr,
+            page_size,
+            PROT_READ | PROT_WRITE | PROT_EXEC,
+        );
+        if ret_val != 0 {
+            log::error!("mprotect ret_val = {ret_val}");
+        }
+    }
+    // Write the jump instructions
+    unsafe {
+        std::ptr::copy_nonoverlapping(
+            patch_instructions.as_ptr(),
+            target_func,
+            patch_instructions.len(),
+        );
+    }
+    // Restore memory protection (if needed)
+    unsafe {
+        let ret_val = mprotect(page_aligned_addr, page_size, PROT_READ | PROT_EXEC);
+        if ret_val != 0 {
+            log::error!("mprotect ret_val = {ret_val}");
+        }
+    }
+}
+
+pub fn backup_original_bytes(address: *const c_void, length: usize) -> Vec<u8> {
+    let bytes = unsafe { std::slice::from_raw_parts(address as *const u8, length) };
+    bytes.to_vec()
+}
