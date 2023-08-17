413d95955a73c85131d0f63d1f5af18fdec260f3
Brandon Ros <brandonros1@gmail.com>
2023-08-17 19:38:20 -0400

    lint

--- examples/libc_hook.rs
+++ examples/libc_hook.rs
@@ -1,40 +1,40 @@
 #![feature(c_variadic)]
 #![allow(non_camel_case_types)]
 
-use std::ffi::*;
-use std::sync::Mutex;
-use arm_retour::hooks::{Hook, self};
+use arm_retour::hooks::{self, Hook};
 use libc::pid_t;
 use minidl::Library;
 use once_cell::sync::Lazy;
+use std::ffi::*;
+use std::sync::Mutex;
 
 use num_derive::FromPrimitive;
 
 #[derive(FromPrimitive, Debug, PartialEq)]
 pub enum __ptrace_request {
-    PTRACE_TRACEME = 0,
-    PTRACE_PEEKTEXT = 1,
-    PTRACE_PEEKDATA = 2,
-    PTRACE_PEEKUSER = 3,
-    PTRACE_POKETEXT = 4,
-    PTRACE_POKEDATA = 5,
-    PTRACE_POKEUSER = 6,
-    PTRACE_CONT = 7,
-    PTRACE_KILL = 8,
-    PTRACE_SINGLESTEP = 9,
-    PTRACE_GETREGS = 12,
-    PTRACE_SETREGS = 13,
-    PTRACE_GETFPREGS = 14,
-    PTRACE_SETFPREGS = 15,
-    PTRACE_ATTACH = 16,
-    PTRACE_DETACH = 17,
-    PTRACE_GETFPXREGS = 18,
-    PTRACE_SETFPXREGS = 19,
-    PTRACE_SYSCALL = 24,
-    PTRACE_SETOPTIONS = 0x4200,
-    PTRACE_GETEVENTMSG = 0x4201,
-    PTRACE_GETSIGINFO = 0x4202,
-    PTRACE_SETSIGINFO = 0x4203
+  PTRACE_TRACEME = 0,
+  PTRACE_PEEKTEXT = 1,
+  PTRACE_PEEKDATA = 2,
+  PTRACE_PEEKUSER = 3,
+  PTRACE_POKETEXT = 4,
+  PTRACE_POKEDATA = 5,
+  PTRACE_POKEUSER = 6,
+  PTRACE_CONT = 7,
+  PTRACE_KILL = 8,
+  PTRACE_SINGLESTEP = 9,
+  PTRACE_GETREGS = 12,
+  PTRACE_SETREGS = 13,
+  PTRACE_GETFPREGS = 14,
+  PTRACE_SETFPREGS = 15,
+  PTRACE_ATTACH = 16,
+  PTRACE_DETACH = 17,
+  PTRACE_GETFPXREGS = 18,
+  PTRACE_SETFPXREGS = 19,
+  PTRACE_SYSCALL = 24,
+  PTRACE_SETOPTIONS = 0x4200,
+  PTRACE_GETEVENTMSG = 0x4201,
+  PTRACE_GETSIGINFO = 0x4202,
+  PTRACE_SETSIGINFO = 0x4203,
 }
 
 type ptrace_fn = unsafe extern "C" fn(request: c_uint, ...) -> c_long;
@@ -44,47 +44,47 @@ static LIBC_PTRACE: Lazy<ptrace_fn> = Lazy::new(|| unsafe { LIBC_HANDLE.sym("ptr
 static PTRACE_HOOK_STRUCT: Lazy<Mutex<Option<Hook>>> = Lazy::new(|| Mutex::new(None));
 
 unsafe extern "C" fn ptrace_hook(request: c_uint, mut args: ...) -> c_long {
-    // TODO: preserve stack/state?
-    let parsed_request: __ptrace_request = num::FromPrimitive::from_u32(request).unwrap();
-    log::info!("ptrace_hook request = {request:x} parsed_request = {parsed_request:?}");
-    // TODO: wait for attach and do special stuff?
-    /*if parsed_request == __ptrace_request::PTRACE_ATTACH {
-        let _ = bugsalot::debugger::wait_until_attached(None);
-        loop {
-            std::thread::sleep(std::time::Duration::from_millis(1000));
-        }
-    }*/
-    let pid: pid_t = unsafe { args.arg() }; // TODO: should this be a pointer or not?
-    let addr: *mut c_void = unsafe { args.arg() };
-    let data: *mut c_void = unsafe { args.arg() };
-    log::info!("ptrace_hook request = {request:x} pid = {pid} addr = {addr:p} data = {data:p}");
-    let guard = PTRACE_HOOK_STRUCT.lock().unwrap();
-    let hook_struct = guard.as_ref().unwrap();
-    let ori_fn_address = hook_struct.ori_fn_address;
-    let ori_fn_bytes = hook_struct.ori_fn_bytes.clone();
-    let patch_fn_bytes = hook_struct.patch_fn_bytes.clone();
-    assert_eq!(ori_fn_bytes.len(), patch_fn_bytes.len());
-    drop(guard);
-    // Restore the original bytes.
-    hooks::disable_hook(ori_fn_address, &ori_fn_bytes);
-    // Call original.
-    let ret_val = LIBC_PTRACE(request, pid, addr, data);
-    // Patch again to point to your hook.
-    hooks::enable_hook(ori_fn_address, &patch_fn_bytes);
-    log::info!("ptrace_hook request = {request:x} parsed_request = {parsed_request:?} pid = {pid} addr = {addr:p} data = {data:p} ret_val = {ret_val}");
-    // TODO: restore stack/registers/state?
-    return ret_val;
-  }
+  // TODO: preserve stack/state?
+  let parsed_request: __ptrace_request = num::FromPrimitive::from_u32(request).unwrap();
+  log::info!("ptrace_hook request = {request:x} parsed_request = {parsed_request:?}");
+  // TODO: wait for attach and do special stuff?
+  /*if parsed_request == __ptrace_request::PTRACE_ATTACH {
+      let _ = bugsalot::debugger::wait_until_attached(None);
+      loop {
+          std::thread::sleep(std::time::Duration::from_millis(1000));
+      }
+  }*/
+  let pid: pid_t = unsafe { args.arg() }; // TODO: should this be a pointer or not?
+  let addr: *mut c_void = unsafe { args.arg() };
+  let data: *mut c_void = unsafe { args.arg() };
+  log::info!("ptrace_hook request = {request:x} pid = {pid} addr = {addr:p} data = {data:p}");
+  let guard = PTRACE_HOOK_STRUCT.lock().unwrap();
+  let hook_struct = guard.as_ref().unwrap();
+  let ori_fn_address = hook_struct.ori_fn_address;
+  let ori_fn_bytes = hook_struct.ori_fn_bytes.clone();
+  let patch_fn_bytes = hook_struct.patch_fn_bytes.clone();
+  assert_eq!(ori_fn_bytes.len(), patch_fn_bytes.len());
+  drop(guard);
+  // Restore the original bytes.
+  hooks::disable_hook(ori_fn_address, &ori_fn_bytes);
+  // Call original.
+  let ret_val = LIBC_PTRACE(request, pid, addr, data);
+  // Patch again to point to your hook.
+  hooks::enable_hook(ori_fn_address, &patch_fn_bytes);
+  log::info!("ptrace_hook request = {request:x} parsed_request = {parsed_request:?} pid = {pid} addr = {addr:p} data = {data:p} ret_val = {ret_val}");
+  // TODO: restore stack/registers/state?
+  return ret_val;
+}
 
-  fn patch_libc_ptrace() {
-    let ori_fn_address = *LIBC_PTRACE as *const c_void;
-    let hook_fn_address = ptrace_hook as *const c_void;
-    let ori_fn_bytes_length = 10; // TODO: do not need this dynamically?
-    let hook = hooks::build_hook(ori_fn_address, hook_fn_address, ori_fn_bytes_length);
-    hooks::enable_hook(hook.ori_fn_address, &hook.patch_fn_bytes);
-    PTRACE_HOOK_STRUCT.lock().unwrap().replace(hook);
+fn patch_libc_ptrace() {
+  let ori_fn_address = *LIBC_PTRACE as *const c_void;
+  let hook_fn_address = ptrace_hook as *const c_void;
+  let ori_fn_bytes_length = 10; // TODO: do not need this dynamically?
+  let hook = hooks::build_hook(ori_fn_address, hook_fn_address, ori_fn_bytes_length);
+  hooks::enable_hook(hook.ori_fn_address, &hook.patch_fn_bytes);
+  PTRACE_HOOK_STRUCT.lock().unwrap().replace(hook);
 }
 
 fn main() {
-    patch_libc_ptrace();
+  patch_libc_ptrace();
 }
--- src/assembly.rs
+++ src/assembly.rs
@@ -39,55 +39,54 @@ bitfield! {
     pub get_opcode2, set_opcode2: 2, 0;
 }
 
-
 fn get_bit_range(input: u16, start: u8, num_bits: u8) -> u16 {
-    let mask = (1u16 << num_bits) - 1;
-    (input >> start) & mask
+  let mask = (1u16 << num_bits) - 1;
+  (input >> start) & mask
 }
 
 pub fn encode_movw(immediate: u16, rd: u8) -> u32 {
-    println!("encode_movw immediate = {immediate:x} rd = {rd:x}");
-    let mut movw = Movw(0);
-    movw.set_op1(0b11110);
-    movw.set_imm1(get_bit_range(immediate, 11, 1) as u32); // 11-11
-    movw.set_op2(0b100100);
-    movw.set_imm4(get_bit_range(immediate, 12, 4) as u32); // 12-15
-    movw.set_imm3(get_bit_range(immediate, 8, 3) as u32); // 8-10
-    movw.set_Rd(rd as u32);
-    movw.set_imm8(get_bit_range(immediate, 0, 8) as u32); // 0-7
-    movw.set_op3(0);
-    return u32::from_le_bytes(movw.0.to_be_bytes());
+  println!("encode_movw immediate = {immediate:x} rd = {rd:x}");
+  let mut movw = Movw(0);
+  movw.set_op1(0b11110);
+  movw.set_imm1(get_bit_range(immediate, 11, 1) as u32); // 11-11
+  movw.set_op2(0b100100);
+  movw.set_imm4(get_bit_range(immediate, 12, 4) as u32); // 12-15
+  movw.set_imm3(get_bit_range(immediate, 8, 3) as u32); // 8-10
+  movw.set_Rd(rd as u32);
+  movw.set_imm8(get_bit_range(immediate, 0, 8) as u32); // 0-7
+  movw.set_op3(0);
+  return u32::from_le_bytes(movw.0.to_be_bytes());
 }
 
 pub fn encode_movt(immediate: u16, rd: u8) -> u32 {
-    println!("encode_movt immediate = {immediate:x} rd = {rd:x}");
-    let mut movt = Movt(0);
-    movt.set_op1(0b11110);
-    movt.set_imm1(get_bit_range(immediate, 11, 1) as u32); // 11-11
-    movt.set_op2(0b101100);
-    movt.set_imm4(get_bit_range(immediate, 12, 4) as u32); // 12-15
-    movt.set_imm3(get_bit_range(immediate, 8, 3) as u32); // 8-10
-    movt.set_Rd(rd as u32);
-    movt.set_imm8(get_bit_range(immediate, 0, 8) as u32); // 0-7
-    movt.set_op3(0);
-    return u32::from_le_bytes(movt.0.to_be_bytes());
+  println!("encode_movt immediate = {immediate:x} rd = {rd:x}");
+  let mut movt = Movt(0);
+  movt.set_op1(0b11110);
+  movt.set_imm1(get_bit_range(immediate, 11, 1) as u32); // 11-11
+  movt.set_op2(0b101100);
+  movt.set_imm4(get_bit_range(immediate, 12, 4) as u32); // 12-15
+  movt.set_imm3(get_bit_range(immediate, 8, 3) as u32); // 8-10
+  movt.set_Rd(rd as u32);
+  movt.set_imm8(get_bit_range(immediate, 0, 8) as u32); // 0-7
+  movt.set_op3(0);
+  return u32::from_le_bytes(movt.0.to_be_bytes());
 }
 
 pub fn encode_blx_register(rm: u8) -> u16 {
-    println!("encode_blx_register rm = {rm:x}");
-    let mut blx_register = BlxRegister(0);
-    blx_register.set_Rm(rm as u16);
-    blx_register.set_opcode1(0b010001111);
-    blx_register.set_opcode2(0);
-    return u16::from_le_bytes(blx_register.0.to_be_bytes());
+  println!("encode_blx_register rm = {rm:x}");
+  let mut blx_register = BlxRegister(0);
+  blx_register.set_Rm(rm as u16);
+  blx_register.set_opcode1(0b010001111);
+  blx_register.set_opcode2(0);
+  return u16::from_le_bytes(blx_register.0.to_be_bytes());
 }
 
 pub fn encode_bx() -> u16 {
-    // TODO: bitfield for clarity + support something other than r12
-    return 0x6047; // bx r12
+  // TODO: bitfield for clarity + support something other than r12
+  return 0x6047; // bx r12
 }
 
 pub fn encode_nop() -> u16 {
-    // TODO: bitfield for clarity?
-    return 0x00bf;
+  // TODO: bitfield for clarity?
+  return 0x00bf;
 }
